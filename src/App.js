fe93decfa14ad97e2f710c06cda9bb19f34694e1
Mr Mask <kr.ankit253@gmail.com>
2022-05-03 18:30:43 +0530

    new changes1

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload. mat karo
+          Edit <code>src/App.js</code> TUSHAR and save to reload. mat karo
         </p>
         <a
           className="App-link"
